d07163376a0bfa46822f208122428c669686f659
Sangelo <contact@sangelo.space>
2024-04-03 16:49:15 +0200

    [a+d] Initial docker-compose + delete old build scripts

--- build-podman.sh
+++
@@ -1,5 +0,0 @@
-podman compose -f docker-compose.build.yml down && \
-#podman compose -f docker-compose.build.yml rm --all && \
-podman compose -f docker-compose.build.yml build --no-cache && \
-podman compose -f docker-compose.build.yml up -d --force-recreate && \
-watch -n 1 podman compose -f docker-compose.build.yml ps
